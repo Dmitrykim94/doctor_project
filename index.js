9c3cbd56d958423a590e6d1a205b2e505df3c92a
Geoge <jkjkj.power@gmail.com>
2019-06-13 10:02:25 +0300

    cra

--- index.js
+++
@@ -1,1 +0,0 @@
-const dima = 'dima'
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                APODJPOADFD
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Index/>, 
+    document.getElementById('root'))
